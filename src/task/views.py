d1175e4a2161756476108daed4bdaad5867a89c8
Marsha1681 <marshaflorence808@gmail.com>
2026-05-18 04:46:22 +0300

    Fixed blanks,duplication errors and added filtering and ordering logic

--- src/task/views.py
+++ src/task/views.py
@@ -1,12 +1,31 @@
 from rest_framework import generics
 from .models import Location
 from .serializers import LocationListSerializer, LocationDetailSerializer
+from django.db.models import Count
 
 
 class LocationListView(generics.ListAPIView):
-    queryset = Location.objects.all()
     serializer_class = LocationListSerializer
+    def get_queryset(self):
+        
+        queryset = Location.objects.all().annotate(
+            number_of_evses=Count("evses")
+        )
 
+        country = self.request.query_params.get("country")
+        operator = self.request.query_params.get("operator")
+
+        if country:
+            queryset = queryset.filter(country_reference=country)
+
+        if operator:
+            queryset = queryset.filter(operator_reference=operator)
+
+        ordering = self.request.query_params.get("ordering")
+        if ordering in ["created_at", "-created_at", "updated_at", "-updated_at"]:
+            queryset = queryset.order_by(ordering)
+
+        return queryset
 
 class LocationDetailView(generics.RetrieveAPIView):
     queryset = Location.objects.all()
