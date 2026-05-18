e893fd9907b4882216a0545847d3fac67389a62b
Marsha1681 <marshaflorence808@gmail.com>
2026-05-18 06:06:23 +0300

    Created an admin view page

--- src/task/admin.py
+++ src/task/admin.py
@@ -1,3 +1,31 @@
-# from django.contrib import admin
+from django.contrib import admin
+from .models import Location, EVSE, Connector
 
-# Register your models here.
+class EVSEInline(admin.TabularInline):
+    model = EVSE
+    extra = 0
+
+class ConnectorInline(admin.TabularInline):
+    model = Connector
+    extra = 0
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "operator_reference",
+        "country_reference",
+        "postal_code",
+        "latitude",
+        "longitude",
+    )
+    inlines = [EVSEInline]
+
+@admin.register(EVSE)
+class EVSEAdmin(admin.ModelAdmin):
+    list_display = ("id", "location", "physical_identifier", "status")
+    inlines = [ConnectorInline]
+
+@admin.register(Connector)
+class ConnectorAdmin(admin.ModelAdmin):
+    list_display = ("id", "evse", "power", "standard")
